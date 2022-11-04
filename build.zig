3fb385fcf36593a6752d407f123341b48f3cc9c1
BlueAlmost <git.blue.almost@gmail.com>
2022-11-04 19:18:05 +0100

    simplified build.zig more

--- build.zig
+++ build.zig
@@ -5,14 +5,6 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    // const exe_targets = [_]Target {
-    //     // .{ .name = "foo", .src = "foo.zig", .desc ="foo"},
-    // };
-
-    // for (exe_targets) |e_target| {
-    //     e_target.build(b);
-    // }
-
     const exe_tests = b.addTest("./main_test.zig");
     exe_tests.addPackagePath("zlinalg", "zlinalg.zig");
     exe_tests.setTarget(target);
