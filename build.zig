3d14a1cc65fbdf441f9fb717cad2c3b592fe4467
BlueAlmost <git.blue.almost@gmail.com>
2022-11-04 19:13:36 +0100

    simplified build.zig

--- build.zig
+++ build.zig
@@ -5,53 +5,20 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const exe_targets = [_]Target {
-        // .{ .name = "foo", .src = "foo.zig", .desc ="foo"},
-    };
+    // const exe_targets = [_]Target {
+    //     // .{ .name = "foo", .src = "foo.zig", .desc ="foo"},
+    // };
 
-    for (exe_targets) |e_target| {
-        e_target.build(b);
-    }
+    // for (exe_targets) |e_target| {
+    //     e_target.build(b);
+    // }
 
     const exe_tests = b.addTest("./main_test.zig");
+    exe_tests.addPackagePath("zlinalg", "zlinalg.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
-
-    exe_tests.addPackagePath("zlinalg", "zlinalg.zig");
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
 }
 
-//---------------------------------------------------------------------------
-
-const Target = struct {
-
-    name: []const u8,
-    src:  []const u8,
-    desc: []const u8,
-
-    pub fn build(self: Target, b: *std.build.Builder) void {
-
-        // const target = b.standardTargetOptions(.{});
-        const mode = b.standardReleaseOptions();
-
-        var exe = b.addExecutable(self.name, self.src);
-        exe.setOutputDir(b.pathFromRoot("./bin"));
-
-
-        exe.setBuildMode(b.standardReleaseOptions());
-        // exe.setTarget(target);
-        exe.setBuildMode(mode);
-
-        exe.addPackagePath("zlinalg", "zlinalg.zig");
-        
-        exe.install();
-
-        const run_cmd = exe.run();
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-    }
-};
